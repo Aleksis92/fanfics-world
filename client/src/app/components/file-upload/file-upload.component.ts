f31da6aa899ae98e8c10527f100e6231bef65c1a
Aleksis92 <Sanya77a@gmail.com>
2018-02-26 13:04:57 +0300

    Fix image upload

--- client/src/app/components/file-upload/file-upload.component.ts
+++ client/src/app/components/file-upload/file-upload.component.ts
@@ -11,14 +11,10 @@ import {FanficService} from '../../services/fanfic.service';
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent {
-  // Main task
   task: AngularFireUploadTask;
-  // Progress monitoring
   percentage: Observable<number>;
   snapshot: Observable<any>;
-  // Download URL
   downloadURL: Observable<string>;
-  // State for dropzone CSS toggling
   isHovering: boolean;
   constructor(
     private storage: AngularFireStorage,
@@ -29,27 +25,19 @@ export class FileUploadComponent {
     this.isHovering = event;
   }
   startUpload(event: FileList) {
-    // The File object
     const file = event.item(0);
-    // Client-side validation example
     if (file.type.split('/')[0] !== 'image') {
       console.error('unsupported file type :( ');
       return;
     }
-    // The storage path
     const path = `test/${new Date().getTime()}_${file.name}`;
-    // Totally optional metadata
     const customMetadata = { app: 'My AngularFire-powered PWA!' };
-    // The main task
     this.task = this.storage.upload(path, file, { customMetadata });
-    // Progress monitoring
     this.percentage = this.task.percentageChanges();
     this.snapshot   = this.task.snapshotChanges();
-    // The file's download URL
     this.fanficService.coverRefresh = true;
     this.downloadURL = this.task.downloadURL();
   }
-  // Determines if the upload task is active
   isActive(snapshot) {
     return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes
   }
